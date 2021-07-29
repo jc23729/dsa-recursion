6b796b016a0c78e8a8d70a0adba20ba3c52be0af
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-29 16:52:18 -0400

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -32,9 +32,12 @@ function isPalindrome(str, idx = 0) {
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+function findIndex(arr, val, idx = 0) {
+  if (idx === arr.length) return -1;
+  if (arr[idx] === val) return idx;
+  return findIndex(arr, val, idx + 1);
 }
+
 
 /** revString: return a copy of a string, but in reverse. */
 
