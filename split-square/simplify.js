1effc4cfe12d0e6254a0156852afe0c1bd9d326e
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-07-29 17:14:21 -0400

    Update simplify.js

--- split-square/simplify.js
+++ split-square/simplify.js
@@ -1,2 +1,9 @@
 /* simplify(s) - simplify redundant splits in a split square */
 
+function simplify(s) {
+    //base case is just an integer
+    if (s ===0 || s === 1) {
+        return s;
+    }
+    
+}
